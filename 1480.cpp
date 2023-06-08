9159a3501d88643503cbe2a666d5a008c860f931
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-06-08 15:49:13 +0600

    prefix sum related problem

---
+++ 1480.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>&v) {    
+        for(int i=1;i<v.size();i++){
+            v[i]=v[i]+v[i-1];
+           
+        }
+       return v;
+    }
+};
